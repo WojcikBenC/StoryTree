b9d779d55f43d84781e1342482579c7b8ef352a8
bcwojcik <bcwojcik@uwm.edu>
2019-11-19 01:12:30 -0600

    Add project files.

---
+++ StoryTree/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StoryTree
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            current.Text = StoryTree.Tree.cursor.current;
+            left.Text = StoryTree.Tree.cursor.left.choice;
+            right.Text = StoryTree.Tree.cursor.right.choice;
+        }
+
+        void LeftClicked(object sender, EventArgs e)
+        {
+            if(StoryTree.Tree.cursor.left != null) StoryTree.Tree.cursor = StoryTree.Tree.cursor.left;
+            current.Text = StoryTree.Tree.cursor.current;
+            if(StoryTree.Tree.cursor.left != null) left.Text = StoryTree.Tree.cursor.left.choice;
+            if(StoryTree.Tree.cursor.right != null) right.Text = StoryTree.Tree.cursor.right.choice;
+        }
+
+        void RightClicked(object sender, EventArgs e)
+        {
+            if (StoryTree.Tree.cursor.right != null) StoryTree.Tree.cursor = StoryTree.Tree.cursor.right;
+            current.Text = StoryTree.Tree.cursor.current;
+            if (StoryTree.Tree.cursor.left != null) left.Text = StoryTree.Tree.cursor.left.choice;
+            if (StoryTree.Tree.cursor.right != null) right.Text = StoryTree.Tree.cursor.right.choice;
+        }
+    }
+}
---
+++ StoryTree/Tree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StoryTree
+{
+    public static class Tree
+    {
+        public class TreeNode
+        {
+            public String choice;
+            public String current;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(String choice, String current)
+            {
+                this.choice = choice;
+                this.current = current;
+            }
+        }
+
+        private static TreeNode root = new TreeNode(null, "You are an adventurer, seeking to find the lost guitar of the temples. Select where to travel to first on your quest: ");
+        public static TreeNode cursor = root;
+
+        public static TreeNode getRoot()
+        {
+            return root;
+        }
+
+        public static void setupTree()
+        {
+            root.left = new TreeNode("A large city.", "You enter the the city, seeing a sprawling market, a busy riverwalk, and a transit center. Do you go to: ");
+            root.right = new TreeNode("An open field.", "You walk to the open field, seeing a farmhouse off in the distance, as well as a shed with a car in it. Do you go to: ");
+        }
+    }
+}
